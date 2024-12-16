24dedf60ddc6f6fabfc39bf689850781bbb7821a
ChenWei <chen158_7@126.com>
2024-12-16 15:49:05 +0800

    husky+lintstage+eslint+prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import pluginReact from 'eslint-plugin-react'
 import pluginPrettier from 'eslint-plugin-prettier/recommended'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import pluginReact from 'eslint-plugin-react'
+import pluginRN from '@react-native/eslint-config'
 
 export default tseslint.config(
   {
@@ -12,6 +13,7 @@ export default tseslint.config(
       pluginJs.configs.recommended,
       ...tseslint.configs.recommended,
       pluginPrettier
+      // pluginRN
       // pluginReact.configs['flat/essential'],
       // '@react-native'
     ]
@@ -20,18 +22,20 @@ export default tseslint.config(
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   {
     plugins: {
+      '@react-native': pluginRN,
+      react: pluginReact,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh
     }
   },
   {
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      '@typescript-eslint/no-var-requires': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      'react-refresh/only-export-components': ['off', { allowConstantExport: true }],
-      'react-hooks/exhaustive-deps': 'warn'
+      // ...reactHooks.configs.recommended.rules,
+      // '@typescript-eslint/no-var-requires': 'off',
+      // '@typescript-eslint/no-unused-vars': 'off',
+      // '@typescript-eslint/no-explicit-any': 'off',
+      // 'react-refresh/only-export-components': ['off', { allowConstantExport: true }],
+      // 'react-hooks/exhaustive-deps': 'warn'
     }
   }
 )
